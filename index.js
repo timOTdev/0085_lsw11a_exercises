8652b75c09f57716615b0ab1b296c1f6bbeee911
Timothy Hoang <timh1203@gmail.com>
2018-10-03 13:09:11 -0400

    Add logger middleware

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+// pull in express
 const express= require('express');
 
+// instantiate your server
 const app = express();
 app.use(express.json());
 
+// middlewares
+const logger = (req, res, next) => {
+  // next will be called in EVERY piece of middleware we build
+  // it will determine when to go, to the next piece of middleware.
+  console.log(`${Date.now()} ${req.method} made to ${req.url}`);
+  next();
+};
+server.use(logger);
+
+// built a GET endpoint to `/` that returns the string hello world to the client.
 app.get('/', (req, res) => {
   res.send('Hello World');
 })
+server.get('/rapha', (req, res) => {
+  res.send('I am Raphael');
+});
 
+// call server.listen w/ a port of your choosing
 const port = 8000;
-app.listen(port, console.log(`===Port ${port} is running===`));
+app.listen(port, () => console.log(`===Port ${port} is running===`));
+
+// hit your port+/ to see "hello wold!"
